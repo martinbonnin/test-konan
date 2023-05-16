9be8b66690945ed93d3c58f4dc065965b9ce373e
Martin Bonnin <martin@mbonnin.net>
2023-05-16 17:16:54 +0200

    add sha1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
+import okio.Buffer
+import okio.buffer
+import okio.source
 import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
 
+buildscript {
+    dependencies {
+        classpath("com.squareup.okio:okio:3.3.0")
+    }
+}
 plugins {
     id("org.jetbrains.kotlin.multiplatform").version("1.9.20-dev-1641")
 }
@@ -22,7 +30,16 @@ tasks.all {
     if (name == "compileAppleMainKotlinMetadata") {
         doLast {
             this as KotlinNativeCompile
-            println(libraries.joinToString("") { it.absolutePath + "\n" })
+
+            val sha1 = libraries.files.filter { it.name == "org.jetbrains.kotlin.native.platform.CoreFoundation" }.single().walk().filter {
+                it.isFile
+            }.sortedBy { it.path }.map {
+                it.source().buffer().readByteString().sha1().hex()
+            }.joinToString("").let {
+                Buffer().writeUtf8(it).sha1().hex()
+            }
+
+            println("sha1=$sha1")
         }
     }
 }
